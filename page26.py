d78aaf10ac206418b75755b8ba3b73268bf3004a
Greiciane93x <greiciane.assis@hotmial.com>
2024-02-14 13:04:56 -0300

    curso de python intensivo - página 27

--- page26.py
+++ page26.py
@@ -15,7 +15,6 @@ print(f'Tweet keys: {tweet_keys}')
 print(f'Tweet values: {tweet_values}')
 print(f'Tweet items: {tweet_items}')
 
-
 var1 = "user" in tweet_keys
 var2 = "user" in tweet
 var3 = "joelgrus" in tweet_keys
@@ -23,3 +22,40 @@ var3 = "joelgrus" in tweet_keys
 print(f'Verdadeiro, mas nao Pythonic: {var1}')
 print(f'forma Pythonic de verificar as chaves: {var2}')
 print(f'Verdadeiro (lento, mas unica forma de verificar): {var3}')
+
+"""As chaves do dicionário devem ser hashtable [com função hashm imutáveis], logo as listas 
+não podem ser usadas como chave, se você precisar de uma chave com várias partes, use uma tupla ou transforme a chave 
+a chave em uma string"""
+
+# defaultdict
+"""Imagine que você deseja contar as palavras de um documento. Uma abordagem óbvia seria criar um dicionário com 
+chaves para palavras e valores para a contagem. Ao verificar cada palavra, você pode incrementar a contagem (se ela
+já estiver no dicionario) ou  adicioná-la ao dicionário (se ela ainda não estiver nele): """
+
+document = "text"
+
+word_counts = {}
+for word in document:
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+
+"""Você também pode usar o método **é melhor pedir perdão do que permissão**, tratando a exceção gerada na pesquisa pela 
+chave ausente"""
+
+word_counts = {}
+for word in document:
+    try:
+        word_counts[word] += 1
+    except KeyError:
+        word_counts[word] = 1
+
+"""Uma terceira abordagem é usar o get, que lida de forma muito interesante com chaves ausentes:"""
+word_counts = {}
+for word in document:
+    previous_count = word_counts.get(word, 0)
+    word_counts[word] = previous_count + 1
+
+"""Mas, como esses métodos são um tanto quanto complicados, o defaultdict é um bom recurso. Embora pareça um dicionário
+comum, quando procuramos uma chave que está contida nele. """
---
+++ page27.py
@@ -0,0 +1,28 @@
+"""O defaultdict diciona um valor para ela usando a função de argumento zero que indicamos ao criá-lo.
+Para usar os defaultdicts, você deve importá-los das collections: """
+from collections import defaultdict
+
+document = "text"
+word_counts = defaultdict(int)
+for word in document:
+    word_counts[word] += 1
+
+"""Esses recursos também são uteis com list, dict e outras funções: """
+dd_list = defaultdict(list)
+dd_list[2].append(1)
+
+print(f'dd_list = {dd_list}')
+
+dd_dict = defaultdict(dict)
+dd_dict["Joel"]["City"] = "Seattle"
+
+print(f'dd_dict = {dd_dict}')
+
+dd_pair = defaultdict(lambda: [0, 0])
+dd_pair[2][1] = 1
+
+print(f'dd_pair = {dd_pair}')
+
+
+"""Isso será útil quando usarmos dicionário para coletar os resultados de alguma chave sem verificar se ela existe a 
+cada operação"""
